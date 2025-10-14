caa371bec094ac33ad88a5d89647e70d1386869e
rar1224 <kochramowicz@gmail.com>
2025-10-14 23:34:07 +0200

    List/grid toggle button

--- travli/src/Nav.jsx
+++ travli/src/Nav.jsx
@@ -1,7 +1,27 @@
+import './Nav.css';
+import * as motion from 'motion/react-client'
+import {useState} from 'react';
+
 function Nav() {
+const [tripView, setTripView] = useState(-1);
+
+function handleChangeView() {
+    setTripView(-tripView);
+    console.log(tripView);
+}
+
     return (
-        <div>
-            Here will be the navigation!
+        <div className='navigation'>
+            <div className='view-toggle'>
+                <span>List</span>
+                <button className='toggle' onClick={handleChangeView} style={{justifyContent: 'flex-' + (tripView > 0 ? 'start' : 'end')}}>
+                    <motion.span className='toggle-ball' layout transition={{type: 'spring', visualDuration: 0.2, bounce: 0.2}}/>
+                </button>
+                <span>Grid</span>
+            </div>
+            <div>Filter</div>
+            <div>Sort</div>
+            <div>Add</div>
         </div>
     );
 }
