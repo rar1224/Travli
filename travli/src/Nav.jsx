3559e32d28fd69228f64da06f30cb9b0280333f9
rar1224 <kochramowicz@gmail.com>
2025-10-15 21:29:22 +0200

    Organize buttons

--- travli/src/Nav.jsx
+++ travli/src/Nav.jsx
@@ -4,10 +4,15 @@ import {useState} from 'react';
 
 function Nav() {
 const [tripView, setTripView] = useState(-1);
+const [organizeView, setOrganizeView] = useState(0);
 
 function handleChangeView() {
     setTripView(-tripView);
-    console.log(tripView);
+}
+
+function handleOrganizeView(i) {
+    if (organizeView == i) setOrganizeView(0);
+    else setOrganizeView(i);
 }
 
     return (
@@ -19,11 +24,29 @@ function handleChangeView() {
                 </button>
                 <span>Grid</span>
             </div>
-            <div>Filter</div>
-            <div>Sort</div>
-            <div>Add</div>
+            <motion.button className='organize-button' onClick={() => handleOrganizeView(1)} style={organizeView == 1 ? enabled : disabled}>
+                Filter
+            </motion.button>
+            <motion.button className='organize-button' onClick={() => handleOrganizeView(2)} style={organizeView == 2 ? enabled : disabled}>
+                Sort
+            </motion.button>
+            <motion.button className='organize-button'>
+                Add
+            </motion.button>
         </div>
     );
 }
 
 export default Nav;
+
+const disabled = {
+    backgroundColor: "#FEFAE0",
+    color: "#626F47",
+    borderStyle: "solid",
+    borderColor: "#626F47"
+}
+
+const enabled = {
+    backgroundColor: "#A4B465",
+    color: "#FEFAE0"
+}
